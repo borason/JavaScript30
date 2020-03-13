a8f1765f97b6e0bb9048b47a0da0b07bf14a9615
borason <bzilla1982@gmail.com>
2020-03-13 13:26:35 -0600

    start clock app

--- 02 - JS and CSS Clock/app.js
+++ 02 - JS and CSS Clock/app.js
@@ -1,5 +1,24 @@
-// Get time
-const now = new Date();
+// // Get time
+// const now = new Date();
+// const seconds = now.getSeconds();
+// const minutes = now.getMinutes();
+// const hour = now.getHours();
+
+// // event handlers
+// const secondHand = document.querySelector(".second-hand");
+// const minuteHand = document.querySelector(".minute-hand");
+// const hourHand = document.querySelector(".hour-hand");
+
+// const secondsDegrees = ((seconds / 60) * 360) + 90;
+//     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
 const seconds = now.getSeconds();
 const minutes = now.getMinutes();
-const hour = now.getHours();
+const hours = now.getHours();
+}
